f221ec789335808495e5213f85b5a0c26ad6a368
Geoff Vrijmoet <geofferyv@gmail.com>
2024-12-23 10:48:21 -0500

    better

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,22 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       {/* Hero Section */}
-      <div className="relative h-[70vh] w-full">
+      <div className="relative w-full" style={{ height: '90vh' }}>
         <Image
           src="https://i.imgur.com/aXgssqc.jpeg"
           alt="Mystikal performing"
           fill
-          className="object-cover brightness-75"
+          className="object-contain object-center"
           priority
         />
-        <div className="absolute inset-0 bg-gradient-to-b from-black/60 to-transparent">
-          <div className="max-w-4xl mx-auto h-full flex flex-col justify-center px-4">
-            
-            <Link href="/game">
-              <Button size="lg" className="text-lg px-8">
-                Play the secret game →
-              </Button>
-            </Link>
-          </div>
+        <div className="absolute inset-0 flex items-end justify-center pb-12">
+          <Link href="/game">
+            <Button size="lg" className="text-lg px-8">
+              Play the secret game →
+            </Button>
+          </Link>
         </div>
       </div>
-
-      
 
       {/* Footer */}
       <footer className="bg-gray-100 py-8">
